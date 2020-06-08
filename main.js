bf240781eaf3b527f74acf07739c8c8627b4307a
Partha <partha.dave@pressassociation.com>
2020-06-08 23:07:56 +0100

    Further Reading Toggle Feature Created

---
+++ main.js
@@ -0,0 +1,11 @@
+$(function(){
+
+$('.open-content').on('click', function(){
+
+  $('.card-front').toggleClass('hide-me');
+    $('.open-content').toggleClass('rotate-me');
+    $('.card-back').toggleClass('reveal-me');
+});
+
+
+});
