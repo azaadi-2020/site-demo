a772c2505be7355a37788bffde7a2252a1871945
Partha <partha.dave@pressassociation.com>
2020-06-09 19:09:34 +0100

    All cards flip feature added

--- main.js
+++ main.js
@@ -3,8 +3,48 @@ $(function(){
 $('.toggle1').on('click', function(){
   $(this).toggleClass("down")  ;
 
-  $('.test').toggleClass('hide-me');
-    $('.test-back').toggleClass('reveal-me');
+  $('.sub-card1').toggleClass('hide-me');
+    $('.sub-card2').toggleClass('reveal-me');
+});
+
+
+$('.toggle2').on('click', function(){
+  $(this).toggleClass("down")  ;
+
+  $('.sub-card3').toggleClass('hide-me');
+    $('.sub-card4').toggleClass('reveal-me');
+});
+
+
+$('.toggle3').on('click', function(){
+  $(this).toggleClass("down")  ;
+
+  $('.sub-card5').toggleClass('hide-me');
+    $('.sub-card6').toggleClass('reveal-me');
+});
+
+
+$('.toggle4').on('click', function(){
+  $(this).toggleClass("down")  ;
+
+  $('.sub-card7').toggleClass('hide-me');
+    $('.sub-card8').toggleClass('reveal-me');
+});
+
+
+$('.toggle5').on('click', function(){
+  $(this).toggleClass("down")  ;
+
+  $('.sub-card9').toggleClass('hide-me');
+    $('.sub-card10').toggleClass('reveal-me');
+});
+
+
+$('.toggle6').on('click', function(){
+  $(this).toggleClass("down")  ;
+
+  $('.sub-card11').toggleClass('hide-me');
+    $('.sub-card12').toggleClass('reveal-me');
 });
 
 
